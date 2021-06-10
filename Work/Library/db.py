e8eb57616fe8a66b048117629c07cf6f07e1e5e8
Даниил Ященко <dsyaschenko@miem.hse.ru>
2021-06-10 18:08:25 +0300

    main db __init__
    main db __init__

---
+++ Work/Library/db.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Сохранение данных в файле
+Бригада 4 БИВ205
+"""
+# Тестовая база
+import pandas as pd
+
+path_main_db = r"../Data/main_db.csv"  #Путь к базе с студентами
+path_fs_db = r"../Data/fs_db.csv"    #Путь к базе с id дисциплин
+path_sh_db = r"../Data/sh_db.csv"    #Путь к базе с id стипендий
+
+
+def base_main:
+    return pd.read_csv(path_main_db)
+
+
+def base_fs:
+    return pd.read_csv(path_fs_db)
+
+
+def base_sh:
+    return pd.read_csv(path_sh_db)
---
+++ Work/Scripts/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Главная функция
+
+
+"""
+import pandas as pd
+
+
+
+
+if (__name__ == "__main__"):
+    df = pd.read_csv('../Data/sh_db.csv')
+    print(df)
